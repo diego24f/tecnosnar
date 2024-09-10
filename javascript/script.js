d86215fb17c12067c4b12a972ac36713ceee8b1e
diego24f <mundotecno_2015@hotmail.com>
2024-09-10 03:22:04 +0200

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+function alertatormenta (estalloviendo) {
+   // if (estalloviendo == true) {
+        //console.log("agarra el paraguas"); }
+}
+
+
+
+let refmytext = document.querySelector("#mytext")
+
+function mostrartexto(){
+    refmytext.innerHTML= "codigo-748759"
+}
+const inputname = document.querySelector ("#inp-nombre");
+const outputtext = document.querySelector ("#salida-saludo");
+function saludar () 
+{
+        const saludocompleto = "hola, un gusto conocerlo. somos tecno snar y estamos a disposicion ¿en que podemos ayudarlo?" + inputname.value; 
+    outputtext.innerHTML = saludocompleto;}
+
+   
+    
+       
